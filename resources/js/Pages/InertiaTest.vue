9cdd99e75741a558c9d466588547b8b85a4a5aac
nakajimateruaki <teruaki1202@gmail.com>
2023-03-01 21:20:29 +0900

    Add Inertia test controller

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    Inertiaテストです。<br>
+    <a href="/">aタグ</a><br>
+    <Link href="/">Link経由です</Link><br>
+    <Link :href="route('inertia.index')">名前付きルート</Link>
+</template>
+
+<style scoped>
+
+</style>
