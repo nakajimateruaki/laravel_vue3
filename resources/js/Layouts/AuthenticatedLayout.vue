2f2dbc5ac08d45ad36104c24df4e1432fafd92e5
nakajimateruaki <teruaki1202@gmail.com>
2023-03-04 18:24:52 +0900

    Mod jsconfig

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -37,6 +37,13 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     Dashboard
                                 </NavLink>
+
+                                <NavLink
+                                    :href="route('items.index')"
+                                    :active="route().current('items.index')"
+                                >
+                                    商品管理
+                                </NavLink>
                             </div>
                         </div>
 
@@ -143,6 +150,13 @@ const showingNavigationDropdown = ref(false);
                             :active="route().current('dashboard')"
                         >
                             Dashboard
+                        </ResponsiveNavLink>
+
+                        <ResponsiveNavLink
+                            :href="route('items.index')"
+                            :active="route().current('items.index')"
+                        >
+                            商品管理
                         </ResponsiveNavLink>
                     </div>
 
