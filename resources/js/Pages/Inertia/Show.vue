1b7b4fcad1abaec630d57cec7d64c3a910240540
nakajimateruaki <teruaki1202@gmail.com>
2023-03-04 16:19:42 +0900

    add test models

--- resources/js/Pages/Inertia/Show.vue
+++ resources/js/Pages/Inertia/Show.vue
@@ -1,11 +1,24 @@
 <script setup>
+import { Inertia } from '@inertiajs/inertia';
+import { Link } from "@inertiajs/inertia-vue3";
 defineProps({
-    id : String
+    id : String,
+    blog: Object
 })
+
+const deleteConfirm = (id) => {
+    console.log(id);
+    Inertia.delete(`/inertia/${id}`, {
+        onBefore:() => confirm('本当に削除しますか？')
+    })
+
+}
 </script>
 
 <template>
     <div>{{ id }}</div>
+    <div>{{ blog.title }}</div>
+    <button @click="deleteConfirm(blog.id)">削除</button>
 </template>
 
 <style scoped>
